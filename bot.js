dff2b7388a25ac33beeb81401b8a6600ae9dbefa
dannyadu <danny_adu@hotmail.com>
2019-08-24 12:50:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,14 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
   try {
+    if (message.content.charAt(0) === '.f') {
       let uppgift = message.content.slice(1);
       let ytsearch = "Fredrik Lindmark Matmatik 5000 4 " + uppgift;
       let video = await youtube.searchVideos(ytsearch);
       let res = "https://www.youtube.com/watch?v=" + video.id;
-      
+
       message.reply(res);
+    }
   } catch (err) {
     message.reply(err.toString());
     console.log("Response function failed!" + err);
