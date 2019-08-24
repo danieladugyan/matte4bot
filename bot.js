06eb7dbe3e3e5ccc87959d31ac4111bcb55fae92
dannyadu <danny_adu@hotmail.com>
2019-08-24 12:35:32 +0200

    Major feature overhaul

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -8,19 +10,25 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('messge', async (message) => {
+client.on('message', async (message) => {
   try {
     if (message.content.charAt(0) === '.') {
       let uppgift = message.content.slice(1);
-      let ytsearch = "Fredrik Lindmark Matte 4 " + uppgift;
+      let ytsearch = "Fredrik Lindmark Matmatik 5000 4 " + uppgift;
       let video = await youtube.searchVideos(ytsearch);
+      let res = "https://www.youtube.com/watch?v=" + video.id;
 
-      message.reply(video);
+      message.reply(res);
+    } else {
+      message.delete(1000);
     }
-  } catch () {
-    console.error("Response function failed!");
+  } catch (err) {
+    message.reply(err.toString());
+    console.log("Response function failed!" + err);
   }
 });
 
 // DO NOT CHANGE
-client.login(process.env.CLIENT_SECRET);
+client.login(process.env.CLIENT_SECRET).catch((err) => {
+  console.error("Login problem: "+err);
+});
